af1d49995a9a0adda61bc91c7dbe6989bea5e42d
Yashwanth Reddy Koram <koramyashwanthr@gmail.com>
2026-06-12 00:30:44 +0530

    first commit for the project

---
+++ src/test/java/com/yashwanth/portfolio/PortfolioManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashwanth.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
